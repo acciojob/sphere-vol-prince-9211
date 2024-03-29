7c267f3b3063501cca1ea3c8b9af67d0a8ead7e7
prince kumar sacxena <86111082+prince-9211@users.noreply.github.com>
2024-03-28 19:54:54 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
+function volume_sphere(event) {
+    event.preventDefault();
+    
+    var radius = parseFloat(document.getElementById("radius").value);
+    
+    if (isNaN(radius) || radius < 0) {
+        document.getElementById("volume").value = "Invalid input";
+    } else {
+        var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+        
+        document.getElementById("volume").value = volume.toFixed(4);
+    }
+}
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = function() {
+    document.getElementById('MyForm').onsubmit = volume_sphere;
+};
